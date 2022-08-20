70823b0b8760aff2516247a8327f0fae961735d9
Anshul Patel <anshulpatel25@gmail.com>
2022-08-20 18:46:02 +0900

    Added cookies

---
+++ assets/js/consent.js
@@ -0,0 +1,16 @@
+window.cookieconsent.initialise({
+  palette: {
+    popup: {
+      background: "#eaf7f7",
+      text: "#5c7291",
+    },
+    button: {
+      background: "#56cbdb",
+      text: "#ffffff",
+    },
+  },
+  theme: "edgeless",
+  content: {
+    href: "https://developers.google.com/analytics/devguides/collection/gtagjs/cookie-usage",
+  },
+});
